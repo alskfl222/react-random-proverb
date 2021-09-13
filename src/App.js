d3cb04793179164549e6dbf3ae1d596685e2b924
alskfl222 <alskfl222@kakao.com>
2021-09-14 08:49:32 +0900

    make test app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const proverbs = ['피할 수 없으면 즐겨라', '행복은 습관이다, 그것을 몸에 지니라', '인생에 뜻을 세우는데 있어 늦은 때라곤 없다']
+  const getRandomIndex = (length) => {
+    return parseInt(Math.random() * length);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>명언</code> 을 보는 앱을 만듭니다
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {proverbs[getRandomIndex(proverbs.length)]}
       </header>
     </div>
   );
